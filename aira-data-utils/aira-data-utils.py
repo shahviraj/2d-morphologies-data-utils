aa6c6b5ebf49d094037b22476f3d39dd304a0b9b
ameya005 <ameya@iastate.edu>
2019-02-25 12:05:13 -0600

    Initial dev commit

--- aira-data-utils/aira-data-utils.py
+++ aira-data-utils/aira-data-utils.py
@@ -1,7 +1,28 @@
+#!/usr/env/bin python3
 """
 Data utilities for AIRA Material Science Dataset
 """
 
-__author__ = "Ameya Joshi, Viraj Shah"
-__email__ == "ameya@iastate.edu"
+import os
+import numpy as np
+import argparse as ap
+import requests
 
+class AIRADataset():
+    """
+    Class to handle the AIRA dataset. 
+    Supports downloading and visualizing the dataset.
+    """
+
+    def __init__(self):
+        pass
+    
+    def download_dataset(self, datadir):
+        pass
+    
+    def show_random_images(self, num_images=16):
+        pass
+    
+    def generate_thumbnails(self, num_images=16):
+        pass
+    
